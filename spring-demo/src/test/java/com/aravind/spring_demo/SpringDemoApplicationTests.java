0ad624e3c926328061cb922fa764cd7a0722e219
aravinds20 <aravinds.cb@gmail.com>
2024-12-02 17:10:06 +0530

    Setting up spring project.

---
+++ spring-demo/src/test/java/com/aravind/spring_demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravind.spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
