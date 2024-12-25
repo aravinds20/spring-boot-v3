136beb434c5b3af636dc1b2f3886a27c46e93b38
aravinds20 <aravinds.cb@gmail.com>
2024-12-25 19:59:37 +0530

    constructor injection.

--- spring-demo/src/main/java/com/aravind/spring_demo/a1/DependencyInjectionLauncherApplication.java
+++ spring-demo/src/main/java/com/aravind/spring_demo/a1/DependencyInjectionLauncherApplication.java
@@ -15,17 +15,26 @@ class mainApp {
 
     private Dependency2 dependency2;
 
-    @Autowired
-    public void setDependency1(Dependency1 dependency1) {
-        System.out.println("setter injection dependency1");
+//    @Autowired is not mandatory. Just a constructor with all the dependencies would enable the spring to use the
+//    constructor to create the object.
+//    @Autowired
+    public mainApp(Dependency1 dependency1, Dependency2 dependency2) {
+        System.out.println("constructor injection.");
         this.dependency1 = dependency1;
-    }
-
-    @Autowired
-    public void setDependency2(Dependency2 dependency2) {
-        System.out.println("setter injection dependency2");
         this.dependency2 = dependency2;
     }
+
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("setter injection dependency1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("setter injection dependency2");
+//        this.dependency2 = dependency2;
+//    }
 
     @Override
     public String toString() {
